cd41cdeff92d2ec626bfef135f2638a3ed372819
Amit Bhushan <amit.bhushan@squadstack.com>
2023-03-15 00:59:57 +0530

    Created a navbar

---
+++ components/navbar.js
@@ -0,0 +1,11 @@
+const style = {
+    wrapper: `h-16 w-full bg-black text-white flex md:justify-around items-center px-60 fixed z-20`,
+}
+
+const navbar = () => {
+  return (
+    <div className={style.wrapper}>navbar</div>
+  )
+}
+
+export default navbar
